3f536b2491f1e5f31c7f00cbf1cb2a39bac4bd23
abdallah <abdallah.yasser0501@gmail.com>
2024-10-18 22:43:45 +0300

    edited eslint again by adding rules-off

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -9,8 +9,6 @@ export interface simpleBlogCard {
 export interface fullBlog {
     currentSlug: string,
     title: string,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     titleImage: any
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     content: any
 }
